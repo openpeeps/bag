b89628b241e7b44830552a0119b91ff450c4fd08
George Lemon <georgelemon@protonmail.com>
2023-02-08 22:05:05 +0200

    add tests
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- tests/test1.nim
+++ tests/test1.nim
@@ -1,12 +1,36 @@
-# This is just an example to get you started. You may wish to put all of your
-# tests into a single file, or separate them into multiple `test1`, `test2`
-# etc. files (better names are recommended, just make sure the name starts with
-# the letter 't').
-#
-# To run these tests, simply execute `nimble test`.
-
-import unittest
-
+import std/[unittest, tables]
 import bag
-test "can add":
-  check add(5, 5) == 10
+
+var errorMessages = toTable({
+  "auth.error.name": "Please provide a name",
+  "auth.error.email": "Invalid email address",
+  "auth.error.password": "Invalid password",
+  "comment.message.empty": "Missing a message",
+  "comment.message.min": "Min 80 characters",
+  "comment.message.max": "Max 120 characters"
+})
+
+proc i18n(key: string): string =
+  result = errorMessages[key]
+
+test "can find errors":
+  var fields = @[
+    ("email", "test@examplecom"),
+    ("password", "123admin"),
+    ("message", "Lorem ipsum something better than that")
+  ]
+
+  newBag fields:
+    text: TText or "auth.error.name"
+    email: TEmail or "auth.error.email"
+    password: TPassword or "auth.error.password":
+      min: 8 or "auth.error.password.min"
+    message: TTextarea or "comment.message.empty":
+      min: 10 or "comment.message.min"
+      max: 20 or "comment.message.max"
+    *remember: TCheckbox
+
+  check Bag.isValid == false
+  check Bag.getErrors[0][1] == "auth.error.email"
+  check Bag.getErrors[1][1] == "auth.error.password"
+  check Bag.getErrors[2][1] == "comment.message.max"
